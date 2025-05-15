4a7e6b4dc32719ccaeab7d63bd00b4580fc924ab
juliascot <juliascott@lclark.edu>
2025-05-15 15:20:34 -0700

    Added modified sigmoid function (and its inverse)

--- 3dg/gan/new_gan.py
+++ 3dg/gan/new_gan.py
@@ -7,6 +7,13 @@ import tensorly as tl
 
 
 
+def special_sigmoid(input: any) -> any:
+    return 1 / (1 + np.exp(-6 * input + 3))
+
+
+def special_sigmoid_inverse(input: any) -> any:
+    return (3 - np.log(1 / input - 1)) / 6
+
 
 def create_dense_tensor(filename: str, rank: int, l2: float) -> np.ndarray:
     initial_tensor = Tensor(filename, is_student_outside=True, is_augmented=True)
@@ -20,5 +27,8 @@ def create_dense_tensor(filename: str, rank: int, l2: float) -> np.ndarray:
     augmented_tensor = np.copy(reconstructed_tensor)
     augmented_tensor = np.vstack((augmented_tensor, augmented_tensor * 0.9, augmented_tensor * 1.1))
 
-    return augmented_tensor
+    return special_sigmoid(augmented_tensor)
+
+
+
 
