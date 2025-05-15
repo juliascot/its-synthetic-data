46c6b0ac90d71376d337089b5767abf51672c282
juliascot <juliascott@lclark.edu>
2025-05-15 15:24:25 -0700

    Removed stuff to put in helper_funcs file

--- 3dg/gan/new_gan.py
+++ 3dg/gan/new_gan.py
@@ -1,33 +1,8 @@
-from tensor import Tensor
 import tensorflow as tf
 from keras import layers, models
-from tensorly.decomposition import parafac
-import numpy as np
-import tensorly as tl
 
 
 
-def special_sigmoid(input: any) -> any:
-    return 1 / (1 + np.exp(-6 * input + 3))
-
-
-def special_sigmoid_inverse(input: any) -> any:
-    return (3 - np.log(1 / input - 1)) / 6
-
-
-def create_dense_tensor(filename: str, rank: int, l2: float) -> np.ndarray:
-    initial_tensor = Tensor(filename, is_student_outside=True, is_augmented=True)
-
-    mask = ~np.isnan(initial_tensor.data_tensor)
-    initial_tensor.data_tensor = np.nan_to_num(initial_tensor.data_tensor)
-
-    weights, factors = parafac(initial_tensor.data_tensor, rank=rank, mask=mask, l2_reg=l2)
-    reconstructed_tensor = tl.kruskal_to_tensor((weights, factors))
-
-    augmented_tensor = np.copy(reconstructed_tensor)
-    augmented_tensor = np.vstack((augmented_tensor, augmented_tensor * 0.9, augmented_tensor * 1.1))
-
-    return special_sigmoid(augmented_tensor)
 
 
 
