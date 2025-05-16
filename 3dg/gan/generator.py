b58f15cf2a9baa4beaed94dcba58275557b842a0
juliascot <juliascott@lclark.edu>
2025-05-16 16:00:18 -0700

    Initial structure from https://www.geeksforgeeks.org/generative-adversarial-networks-gans-in-pytorch/

--- 3dg/gan/generator.py
+++ 3dg/gan/generator.py
@@ -5,3 +5,26 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
+from helper_funcs import special_sigmoid, special_sigmoid_inverse
+
+# Generator
+class Generator(nn.Module):
+    def __init__(self, noise_dim):
+        super(Generator, self).__init__()
+        self.noise_dim = noise_dim
+        self.main = nn.Sequential(
+            nn.Linear(noise_dim, 7 * 7 * 256),
+            nn.ReLU(True),
+            nn.Unflatten(1, (256, 7, 7)),
+            nn.ConvTranspose2d(256, 128, 5, stride=1, padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 5, stride=2, padding=2, output_padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 1, 5, stride=2, padding=2, output_padding=1),
+            special_sigmoid()
+        )
+
+    def forward(self, x):
+        return self.main(x)
