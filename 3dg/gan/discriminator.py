47fc085fa64517d013bac3d92d8f1817d3529f38
juliascot <juliascott@lclark.edu>
2025-05-19 10:05:11 -0700

    Added starter code from https://www.geeksforgeeks.org/generative-adversarial-networks-gans-in-pytorch/

--- 3dg/gan/discriminator.py
+++ 3dg/gan/discriminator.py
@@ -6,3 +6,22 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 from helper_funcs import special_sigmoid, special_sigmoid_inverse
+
+
+# Discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 64, 5, stride=2, padding=2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 5, stride=2, padding=2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Flatten(),
+            nn.Linear(7 * 7 * 128, 1)
+        )
+
+    def forward(self, x):
+        return self.main(x)
