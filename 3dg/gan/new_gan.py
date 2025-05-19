f2ba9348f3734eae172a8a13c6f15275fe64c3d3
juliascot <juliascott@lclark.edu>
2025-05-19 16:27:17 -0700

    Added some hyperparameters

--- 3dg/gan/new_gan.py
+++ 3dg/gan/new_gan.py
@@ -10,5 +10,9 @@ from discriminator import Discriminator
 from helper_funcs import *
 
 
-
+filename = "Getting_Started_Reordered.csv"
+rank = 5
+augmentation_values = [0.9, 1.1] # Multiply the whole reconstructed tensor by these amounts to increase its size
+l2 = 0
+epochs = 1000
 
