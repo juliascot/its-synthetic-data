09851af880e5092088bcb4a80e924bdc803c72f7
juliascot <juliascott@lclark.edu>
2025-05-19 10:43:46 -0700

    Adjusted dimensions for our data size (18 x 83)

--- 3dg/gan/discriminator.py
+++ 3dg/gan/discriminator.py
@@ -13,14 +13,14 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            nn.Conv2d(1, 64, 5, stride=2, padding=2),
+            nn.Conv2d(1, 64, 3, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
             nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, 5, stride=2, padding=2),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
             nn.BatchNorm2d(128),
             nn.Flatten(),
-            nn.Linear(7 * 7 * 128, 1)
+            nn.Linear(5 * 21 * 256, 1)
         )
 
     def forward(self, x):
