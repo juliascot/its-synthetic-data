20a058fc3551d043c459aef20f25210d3d325eab
juliascot <juliascott@lclark.edu>
2025-05-19 10:46:18 -0700

    Deleted extra imports

--- 3dg/gan/discriminator.py
+++ 3dg/gan/discriminator.py
@@ -1,11 +1,4 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
-from helper_funcs import special_sigmoid, special_sigmoid_inverse
 
 
 # Discriminator
