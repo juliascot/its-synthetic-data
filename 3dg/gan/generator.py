dd4c5d31570965bab62cdf02603fef302b2217c4
juliascot <juliascott@lclark.edu>
2025-05-19 10:40:00 -0700

    Adjusted the dimensions to work better with the discriminator

--- 3dg/gan/generator.py
+++ 3dg/gan/generator.py
@@ -13,16 +13,16 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.noise_dim = noise_dim
         self.main = nn.Sequential(
-            nn.Linear(noise_dim, 4 * 20 * 256),
+            nn.Linear(noise_dim, 5 * 21 * 256),
             nn.ReLU(True),
-            nn.Unflatten(1, (256, 4, 20)),
+            nn.Unflatten(1, (256, 5, 21)),
             nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 1, 3, stride=2, padding=1, output_padding=(0,1)),
+            nn.ConvTranspose2d(64, 1, 3, stride=2, padding=1, output_padding=(1,2)),
             special_sigmoid()
         )
 
