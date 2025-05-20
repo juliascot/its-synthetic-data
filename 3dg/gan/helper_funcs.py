df5090c2b1b7a98fe34b5e4b7306102e30be1cdb
juliascot <juliascott@lclark.edu>
2025-05-20 11:45:29 -0700

    Gradient penalty starter code from https://www.digitalocean.com/community/tutorials/wgans

--- 3dg/gan/helper_funcs.py
+++ 3dg/gan/helper_funcs.py
@@ -31,3 +31,19 @@ def create_dense_tensor(
     augmented_tensor = np.vstack([augmented_tensor] + [val * augmented_tensor for val in post_reconstruction_augmentation_values])
 
     return special_sigmoid(augmented_tensor)
+
+
+def gradient_penalty(self, batch_size, real_images, fake_images):
+        # Get the interpolated image
+        alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0)
+        diff = fake_images - real_images
+        interpolated = real_images + alpha * diff
+
+        with tf.GradientTape() as gp_tape:
+            gp_tape.watch(interpolated)
+            pred = self.discriminator(interpolated, training=True)
+
+        grads = gp_tape.gradient(pred, [interpolated])[0]
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+        gp = tf.reduce_mean((norm - 1.0) ** 2)
+        return gp
