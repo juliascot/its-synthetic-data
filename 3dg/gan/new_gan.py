af61d924cbec01caacff3b00df4719789ec10605
juliascot <juliascott@lclark.edu>
2025-05-20 09:22:46 -0700

    Added main function and more hyperparameters

--- 3dg/gan/new_gan.py
+++ 3dg/gan/new_gan.py
@@ -12,7 +12,17 @@ from helper_funcs import *
 
 filename = "Getting_Started_Reordered.csv"
 rank = 5
-augmentation_values = [0.9, 1.1] # Multiply the whole reconstructed tensor by these amounts to increase its size
+pre_reconstruction_augmentation_values = [-2, -1, 1, 2] # Offset first correct attempt on every question by this amount
+post_reconstruction_augmentation_values = [0.9, 1.1] # Multiply the whole reconstructed tensor by these amounts to increase its size
 l2 = 0
 epochs = 1000
+noise_dimension = 100
+
+
+
+if __name__ == "__main__":
+    augmented_tensor = create_dense_tensor(filename, rank)
+
+
+
 
