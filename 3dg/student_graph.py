8b1fa05d0b3f0c05c7d8d8cfd6132c1f620b6940
juliascot <juliascott@lclark.edu>
2025-05-23 09:29:57 -0700

    Removed stuff now in the learning_curve_analysis package

--- 3dg/student_graph.py
+++ 3dg/student_graph.py
@@ -8,41 +8,12 @@ import numpy as np
 import tensorly as tl
 import matplotlib.pyplot as plt
 from tensorly.decomposition import parafac
-from scipy.optimize import curve_fit
 from tensor_import.tensor import Tensor
+from learning_curve_analysis.learning_curve_analysis import extract_prior_and_acquired_knowledge
 
 filename = "Getting_Started_Reordered.csv"
 rank = 8
 l2 = 0
-
-
-# Helper function
-def power_law(x, a, b):
-    return a * np.power(x, b)
-
-
-def extract_prior_and_acquired_knowledge(tensor: np.ndarray) -> list[list[float]]:
-    # Extract prior knowledge (a) and acquired knowledge (b)
-    all_extracted_info = []
-
-    for student_num, student_matrix in enumerate(tensor):
-
-        extracted_info_a = []
-        extracted_info_b = []
-
-        for question in student_matrix:
-
-            X = np.arange(1, len(question) + 1)
-
-            popt, pcov = curve_fit(power_law, X, question, p0=[1, 1], bounds=([0, 0], [1, 1]))
-
-            extracted_info_a.append(popt[0])
-            extracted_info_b.append(popt[1])
-
-        
-        all_extracted_info.append([extracted_info_a, extracted_info_b])
-
-    return all_extracted_info
 
 
 if __name__ == "__main__":
