971fa33a5e136bb8329f162064c6bb5fcd44973d
juliascot <juliascott@lclark.edu>
2025-05-28 09:52:48 -0700

    Renamed variables in extract_prior_and_acquired_knowledge to abstract away from student always being the outside dimension

--- 3dg/internal_imports/learning_curve_analysis.py
+++ 3dg/internal_imports/learning_curve_analysis.py
@@ -11,16 +11,16 @@ def extract_prior_and_acquired_knowledge(tensor: np.ndarray) -> list[list[float]
     # Extract prior knowledge (a) and acquired knowledge (b)
     all_extracted_info = []
 
-    for student_num, student_matrix in enumerate(tensor):
+    for outside_matrix in tensor:
 
         extracted_info_a = []
         extracted_info_b = []
 
-        for question in student_matrix:
+        for inner_fiber in outside_matrix:
 
-            X = np.arange(1, len(question) + 1)
+            X = np.arange(1, len(inner_fiber) + 1)
 
-            popt, pcov = curve_fit(power_law, X, question, p0=[1, 1], bounds=([0, 0], [1, 1]))
+            popt, pcov = curve_fit(power_law, X, inner_fiber, p0=[1, 1], bounds=([0, 0], [1, 1]))
 
             extracted_info_a.append(popt[0])
             extracted_info_b.append(popt[1])
