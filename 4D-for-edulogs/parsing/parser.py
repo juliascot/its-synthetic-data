0273e45c1f318681b1dfd94bec09a82de0a694fa
juliascot <juliascott@lclark.edu>
2025-05-30 11:17:33 -0700

    Outline what the parsing/processing needs to do

---
+++ 4D-for-edulogs/parsing/parser.py
@@ -0,0 +1,11 @@
+# FIRST PASS THROUGH: put timestamps, user number, commands in
+# Organize milestones, consider compromises
+# Manually go through and mark up which milestone I believe students were working on (using EDUQuestions, their logs, and milestones)
+# Write ML program/use SLM to classify which ones
+# Compare the results qualitatively and quantitatively
+
+# SECOND PASS THROUGH: use standardized program to say which milestone each student was working towards, marking if they got it right or wrong (retain commands and doublecheck)
+
+# THIRD PASS THROUGH: number the attempts on each milestone according to timestamp (removing commands)
+
+# FOURTH PASS THROUGH: rearrange students for better anonymization
