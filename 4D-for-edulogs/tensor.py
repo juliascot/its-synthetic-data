c0d6a2b11ab82ff6acc47a6466aea8ccabc6778f
juliascot <juliascott@lclark.edu>
2025-06-05 11:32:29 -0700

    Adjust the orig_present_points to only include array of [student, milestone] points (rather than 3D)

--- 4D-for-edulogs/tensor.py
+++ 4D-for-edulogs/tensor.py
@@ -44,6 +44,6 @@ class Tensor:
                 shaped_data[data[outside_id][row]][data[middle_id][row]][2] = 1
 
 
-        self.orig_present_points = np.array(np.where(~np.isnan(shaped_data))).T # a 2D array of coordinates of data points that exist [[0 1 0] [3 28 17] ...]
+        self.orig_present_points = np.array(np.where(~np.isnan(shaped_data[:, :, 0]))).T # a 2D array of coordinates of data points that exist [[0 1] [3 28] ...]
         self.max_time = max(shaped_data[:, :, 0])
         self.data_tensor = tl.tensor(shaped_data, dtype=tl.float32)
